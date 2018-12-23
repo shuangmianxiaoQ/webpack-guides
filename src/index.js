fa8692a33d8877d8fc783221cfd2ce5142a131de
shuangmianxiaoQ <1245814811@qq.com>
2018-12-23 22:38:17 +0800

    webpack指南：管理输出

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
