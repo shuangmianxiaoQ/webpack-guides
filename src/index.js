f463395195ecd038833242bf1c53595f0b60b2d5
shuangmianxiaoQ <1245814811@qq.com>
2018-12-23 23:29:34 +0800

    webpack指南：模块热替换

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './styles.css';
 
 function component() {
   var element = document.createElement('div');
@@ -8,7 +9,7 @@ function component() {
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+  btn.onclick = printMe; // onclick 事件绑定原始的 printMe 函数上
 
   element.appendChild(btn);
 
@@ -16,3 +17,16 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeAttributeNode(element);
+    element = component(); // 重新渲染页面后，component 更新 click 事件处理
+    document.body.appendChild(element);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: '管理输出'
-    })
-  ]
+      title: '模块热替换'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
