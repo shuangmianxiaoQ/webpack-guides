77160872a39f8c3ca519253d1324da7dcdf4e939
YourName <1245814811@qq.com>
2018-12-24 20:08:49 +0800

    webpack指南：tree shaking

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
+import { cube } from './math';
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  var element = document.createElement('pre');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe; // onclick 事件绑定原始的 printMe 函数上
-
-  element.appendChild(btn);
+  element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join(
+    '\n\n'
+  );
 
   return element;
 }
 
-// document.body.appendChild(component());
-
-let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    // printMe();
-    document.body.removeAttributeNode(element);
-    element = component(); // 重新渲染页面后，component 更新 click 事件处理
-    document.body.appendChild(element);
-  });
-}
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     app: './src/index.js'
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    disableHostCheck: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: '模块热替换'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
